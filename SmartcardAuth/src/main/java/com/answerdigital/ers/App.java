9529db0db44d8540cac2975fff8d475f1bbb2f5a
Aled Greenhalgh <aled.greenhalgh@answerdigital.com>
2023-03-19 22:53:45 +0000

    Start the app

---
+++ SmartcardAuth/src/main/java/com/answerdigital/ers/App.java
@@ -0,0 +1,11 @@
+package com.answerdigital.ers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
