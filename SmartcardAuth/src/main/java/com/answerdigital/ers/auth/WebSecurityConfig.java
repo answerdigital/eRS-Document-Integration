0d34bfb02be931527976b29f71092769b65031ce
Aled Greenhalgh <aled.greenhalgh@answerdigital.com>
2023-03-27 09:33:13 +0100

    Adding user roles

--- SmartcardAuth/src/main/java/com/answerdigital/ers/auth/WebSecurityConfig.java
+++ SmartcardAuth/src/main/java/com/answerdigital/ers/auth/WebSecurityConfig.java
@@ -4,7 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
@@ -20,5 +30,31 @@ public class WebSecurityConfig {
                 .oauth2Login();
 
         return http.build();
+    }
+
+    private OAuth2UserService<OAuth2UserRequest, OAuth2User> oidcUserService() {
+        final OAuth2UserService delegate = new DefaultOAuth2UserService();
+
+        return (userRequest) -> {
+            OAuth2User user = delegate.loadUser(userRequest);
+
+            OAuth2AccessToken accessToken = userRequest.getAccessToken();
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+
+
+
+            Object rbac = user.getAttributes().get("nationalrbacaccess");
+
+
+
+            // TODO
+            // 1) Fetch the authority information from the protected resource using accessToken
+            // 2) Map the authority information to one or more GrantedAuthority's and add it to mappedAuthorities
+
+            // 3) Create a copy of oidcUser but use the mappedAuthorities instead
+            user = new DefaultOAuth2User(mappedAuthorities, user.getAttributes(), "name");
+
+            return user;
+        };
     }
 }
