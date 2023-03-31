278264bf2143361336b318404dab546ae8794a8b
Aled Greenhalgh <aled.greenhalgh@answerdigital.com>
2023-03-31 17:06:22 +0100

    Added configurable http->https redirect. Moving config from application.yml to env

---
+++ SmartcardAuth/src/main/java/com/answerdigital/ers/controllers/HttpsRedirect.java
@@ -0,0 +1,47 @@
+package com.answerdigital.ers.controllers;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HttpsRedirect {
+    @Value("${server.http-port:80}")
+    private int serverPortHttp;
+
+    @Value("${server.port:443}")
+    private int serverPortHttps;
+    @Bean
+    @ConditionalOnProperty(prefix = "server", name = "enable-tls", havingValue = "true")
+    public TomcatServletWebServerFactory factory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+
+        return tomcat;
+    }
+
+    private Connector createHttpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setPort(serverPortHttp);
+        connector.setRedirectPort(serverPortHttps);
+        return connector;
+    }
+}
