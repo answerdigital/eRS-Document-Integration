d82c06ae7d27ac8fd8d3a20e64fa4222290a3a98
Aled Greenhalgh <aled.greenhalgh@answerdigital.com>
2023-04-13 13:50:46 +0100

    Adding release script

---
+++ release.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -x
+
+RELEASE_TAG=v0.0.2
+
+rm -rf target
+
+mkdir target
+
+cd Integration
+zip ../target/Integration-$RELEASE_TAG.zip *
+cd -
+
+cp SmartcardAuth/target/ers-user-auth-app-$RELEASE_TAG.jar target/
+
+mkdir target/WindowsServiceDeployment
+
+cp SmartcardAuth/WindowsServiceDeployment/* target/WindowsServiceDeployment
+cp SmartcardAuth/target/ers-user-auth-app-$RELEASE_TAG.jar target/WindowsServiceDeployment
+
+cd target/WindowsServiceDeployment
+zip ../WindowsServiceDeployment-$RELEASE_TAG.zip *
+cd -
+
+mkdir target/DocumentReviewApplication
+cp -r Client/build/* target/DocumentReviewApplication/ 
+mkdir target/DocumentReviewApplication/app
+cp -r API/out/* target/DocumentReviewApplication/app
+
+cd target/DocumentReviewApplication
+zip -r ../DocumentReviewApplication-$RELEASE_TAG.zip *
+cd -
+
+cd SQL
+zip -r ../target/SQL-$RELEASE_TAG.zip *
+cd -
