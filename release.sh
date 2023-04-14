c22a328671d54ef3cd65cb8547a5f5449cf36e78
Aled Greenhalgh <aled.greenhalgh@answerdigital.com>
2023-04-14 09:13:19 +0100

    Package artefacts, refactor release scripts to be able to use gh-actions structure

--- release.sh
+++ release.sh
@@ -12,21 +12,21 @@ cd Integration
 zip ../target/Integration-$RELEASE_TAG.zip *
 cd -
 
-cp SmartcardAuth/target/ers-user-auth-app-$RELEASE_TAG.jar target/
+cp SmartcardAuth-Build/ers-user-auth-app-$RELEASE_TAG.jar target/
 
 mkdir target/WindowsServiceDeployment
 
 cp SmartcardAuth/WindowsServiceDeployment/* target/WindowsServiceDeployment
-cp SmartcardAuth/target/ers-user-auth-app-$RELEASE_TAG.jar target/WindowsServiceDeployment
+cp SmartcardAuth-Build/ers-user-auth-app-$RELEASE_TAG.jar target/WindowsServiceDeployment
 
 cd target/WindowsServiceDeployment
 zip ../WindowsServiceDeployment-$RELEASE_TAG.zip *
 cd -
 
 mkdir target/DocumentReviewApplication
-cp -r Client/build/* target/DocumentReviewApplication/ 
+cp -r Client-Build/* target/DocumentReviewApplication/ 
 mkdir target/DocumentReviewApplication/app
-cp -r API/out/* target/DocumentReviewApplication/app
+cp -r API-Build/* target/DocumentReviewApplication/app
 
 cd target/DocumentReviewApplication
 zip -r ../DocumentReviewApplication-$RELEASE_TAG.zip *
