2afaeb88d724e232dff6952ed8b568c8658bac8d
Aled Greenhalgh <aled.greenhalgh@answerdigital.com>
2024-04-09 17:50:33 +0100

    Adding response logging. Adding Acceptable use policy. Uplifting version

--- SmartcardAuth/src/main/java/com/answerdigital/ers/auth/WebSecurityConfig.java
+++ SmartcardAuth/src/main/java/com/answerdigital/ers/auth/WebSecurityConfig.java
@@ -1,17 +1,19 @@
 package com.answerdigital.ers.auth;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import com.answerdigital.ers.logging.LoggingInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.*;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -19,14 +21,17 @@ import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
 
-import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class);
 
     private String orgId = System.getenv("ODS_CODE");
 
@@ -35,12 +40,12 @@ public class WebSecurityConfig {
     SERVICE_PROVIDER_CLINICIAN - R0050
     SERVICE_PROVIDER_CLINICIAN_ADMIN - R5170
      */
-    private List<String> userRoleCodes = Arrays.asList("R0050", "R5170");
+    private List<String> userRoleCodes = Arrays.asList("R0050", "R5170", "R8008");
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .antMatchers("/", "/home", "/error", "/oauth2/authorization/**", "/webjars/**", "/assets/**", "/js/**", "/css/**").permitAll()
+                        .antMatchers("/", "/home", "/error", "/oauth2/authorization/**", "/webjars/**", "/assets/**", "/js/**", "/css/**", "/docs/**").permitAll()
                         .antMatchers("/handover").hasRole("ERS_ADMIN_USER")
                         .anyRequest().authenticated()
                 )
@@ -53,8 +58,20 @@ public class WebSecurityConfig {
         final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
 
         return (userRequest) -> {
-
-            RestTemplate userInfoTemplate = new RestTemplate();
+            RestTemplate userInfoTemplate = null;
+            if (logger.isDebugEnabled()){
+                ClientHttpRequestFactory httpFactory = new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory());
+                userInfoTemplate = new RestTemplate(httpFactory);
+                List<ClientHttpRequestInterceptor> interceptors = userInfoTemplate.getInterceptors();
+                if (CollectionUtils.isEmpty(interceptors)) {
+                    interceptors = new ArrayList<>();
+                }
+                interceptors.add(new LoggingInterceptor());
+                userInfoTemplate.setInterceptors(interceptors);
+            } else {
+                userInfoTemplate = new RestTemplate();
+            }
+            //TODO: add logout behaviour
             //userInfoTemplate.setErrorHandler(delegate.);
             delegate.setRestOperations(userInfoTemplate);
             OAuth2User user = delegate.loadUser(userRequest);
@@ -67,7 +84,6 @@ public class WebSecurityConfig {
             String userInfoUrl = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUri();
             ResponseEntity<Cis2User> userInfoResponse = userInfoTemplate.exchange(userInfoUrl, HttpMethod.GET, requestEntity, Cis2User.class);
             Cis2User cis2User = userInfoResponse.getBody();
-
             Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
 
             if (cis2User.getRoles().stream().anyMatch(
@@ -79,7 +95,10 @@ public class WebSecurityConfig {
                             )
             )){
                 mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_ERS_ADMIN_USER"));
-            };
+            }
+            logger.debug("User Selected Role: {}", cis2User.getSelectedRoleId());
+            logger.debug("Roles: {}", cis2User.getRoles().stream().map(Cis2RbacRole::getRoleCode).collect(Collectors.joining(", ")));
+
             String usernameAttribute = userRequest.getClientRegistration()
                     .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
             user = new DefaultOAuth2User(mappedAuthorities, user.getAttributes(), usernameAttribute);
